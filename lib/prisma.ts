b61ab29b58bedc8976dc9c88526e0b59f89a45d3
Dav1dm0 <morohunfoladavid@gmail.com>
2025-09-17 21:54:28 +0100

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
-// ensures a new client is created for each serverless function invocation
-const prisma = new PrismaClient();
+// This ensures there is only one Prisma Client instance.
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export { prisma };
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL,
+            },
+        },
+    });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
